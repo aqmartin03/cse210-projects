64e510cdfe2a4eafa347bf26b41b4cc3942aa98f
Avery Martin <avery.q.martin@gmail.com>
2026-09-16 13:47:30 -0600

    W01 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string letter = "";
+        string sign = "";
+        if (percentage >= 90)
+        {
+            if (percentage < 93)
+            {
+                sign = "-";
+            }
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            if (percentage >= 87)
+            {
+                sign = "+";
+            }
+            else if (percentage < 83)
+            {
+                sign = "-";
+            }
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            if (percentage >= 77)
+            {
+                sign = "+";
+            }
+            else if (percentage < 73)
+            {
+                sign = "-";
+            }
+            letter = "C";
+        }
+        else if (percentage >- 60)
+        {
+            if (percentage >= 67)
+            {
+                sign = "+";
+            }
+            else if (percentage < 63)
+            {
+                sign = "-";
+            }
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        string together = letter + sign;
+        Console.WriteLine($"You got a {together}.");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass the class. Better luck next time.");
+        }
     }
 }
