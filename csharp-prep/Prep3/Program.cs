05e212ee70e627c624063749f2ad5159672bce30
Avery Martin <avery.q.martin@gmail.com>
2026-09-19 18:47:43 -0600

    W02 Prove

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random numberGenerator = new Random();
+        int magic = numberGenerator.Next(1, 101);
+        int guess = -1;
+        int guessAmount = 0;
+        while (magic != guess)
+        {
+            Console.Write("What is your guess? ");
+            string userInput2 = Console.ReadLine();
+            guessAmount ++;
+            guess = int.Parse(userInput2);
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed It!");
+                Console.WriteLine($"You had a total of {guessAmount} guesses.");
+            }
+        }
     }
 }
