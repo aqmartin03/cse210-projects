d31f0388b74223d05c3864c5afc56385c2ac0d0b
Avery Martin <avery.q.martin@gmail.com>
2026-09-21 21:03:49 -0600

    W02 Prepare

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int userNum = -1;
+        int sum = 0;
+        while (userNum != 0)
+        {
+            Console.Write("Enter number: ");
+            string num = Console.ReadLine();
+            userNum = int.Parse(num);
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+        }
+        // Calculate the Sum
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        // Calculate the Average
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        // Calculate the Max
+        int max = 0;
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+        // Stretch: Find the smallest positive number
+        int smallPos = int.MaxValue;
+        foreach (int number in numbers)
+        {
+            if (number > 0)
+            {
+                if (number < smallPos)
+                {
+                    smallPos = number;
+                }
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {smallPos}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int birthYear;
+        PromptUserBirthYear(out birthYear);
+        int squared = SquareNumber(num);
+        DisplayResult(name, squared, birthYear);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string number = Console.ReadLine();
+        int num = int.Parse(number);
+        return num;
+    }
+
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please enter the year you were born: ");
+        year = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int favNum)
+    {
+        int square = favNum * favNum;
+        return square;
+    }
+
+    static void DisplayResult(string userName, int squaredNum, int birthYear)
+    {
+        int age = 2026 - birthYear;
+        Console.WriteLine($"{userName}, the square of your number is {squaredNum}");
+        Console.WriteLine($"{userName}, you will turn {age} this year.");
     }
 }
